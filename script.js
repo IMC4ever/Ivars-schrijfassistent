3ad1f75b4188f63ff89a0b823060f153097fd660
IMC4ever <ivar@ivars.nl>
2025-06-03 17:03:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function checkStatus() {
+  const statusEl = document.getElementById("status");
+  statusEl.textContent = "🔍 AI-logica nog niet verbonden...";
+  statusEl.style.color = "#e37400";
+}
